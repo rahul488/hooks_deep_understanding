b60d9eb092ae3e456b5cc134129990d215de24f4
rahul488 <myself.rahul78@gmail.com>
2023-05-05 20:06:14 +0530

    modify readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # hooks_deep_understanding
 Created with CodeSandbox
+
+
+# useEffect behaviour on strict mode
+called the clean function on first time inside useffect
+If we remove strict mode it will behave as expected i.e no clean uo function is not called in first time when the page is render
+
+# useEffect clean up function beahviour on dependency array change
+Clean up function is called every time if the depenedcy array is change
+
--- src/useTest.js
+++ src/useTest.js
@@ -6,8 +6,14 @@ const useTest = (isTest) => {
   useEffect(() => {
     let x = 4;
     setTest(Math.random() * 2);
+    console.log("normal---");
+    const handler = setTimeout(() => {
+      console.log("called");
+    }, [1000]);
     return () => {
       x = 0;
+      //if we remove clear time out even if cleaner function get called but the setTimeout will execute
+      clearTimeout(handler);
       console.log("unmountedd.....");
     };
   }, [isTest]);
